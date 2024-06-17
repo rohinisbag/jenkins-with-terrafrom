547cef82e11dd8097648a45624cb90648c379907
rohinisbag <149901855+rohinisbag@users.noreply.github.com>
2024-06-17 19:10:36 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "intro" {
+  ami                    = "ami-001843b876406202a"
+  instance_type          = "t2.micro"
+  availability_zone      = "ap-south-1a"
+  key_name               = "dove_key"
+  vpc_security_group_ids = ["sg-02a108efb4a34e9fc"]
+  tags = {
+    Name        = "excerise1-instance",
+    pagal_aurat = "Rohini"
+  }
+
+}
